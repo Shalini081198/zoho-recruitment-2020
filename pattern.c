04cfacabdea6e9242a0d4311345389cef746ae08
Shalini081198 <76002734+Shalini081198@users.noreply.github.com>
2020-12-15 10:23:02 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,i,j;
+printf("Enter the n value\n");
+scanf("%d",n);
+for(i=1;i<=n+1;i++)
+{
+for(j=1;j<=n+1-i;j++)
+printf(" ");
+
+for(j=1;j<=i;j++)
+printf("%d",i-j);
+
+for(j=1;j<=i-1;j++)
+printf("%d",j);
+
+printf("\n");
+}
+}
+
+
+
